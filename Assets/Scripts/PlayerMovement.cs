008ea5543efcb876f72ee3565b28b0fc8f959711
Λsh Dev <69819379+ash-dvlpr@users.noreply.github.com>
2023-11-16 22:18:37 +0100

    WIP player controller v0.2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(PlayerController), typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
@@ -15,17 +15,18 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField] private float moveForce = 20f;
 
     [Header("Jump")]
-    //[SerializeField] private float jumpForce = 10f;
-    //[SerializeField] private float jumpGravity = 1f;
-    //[SerializeField] private float fallGravity = 3f;
+    [SerializeField] private float jumpForce   = 10f;
+    [SerializeField] private float maxJumpTime = 10f;
+    [SerializeField] private float jumpGravity = 1f;
+    [SerializeField] private float fallGravity = 3f;
     [SerializeField] private float groundCheckRange = 2.2f;
 
     //? Variables
-    private bool canMove = true, isGrounded;
+    private bool canMove = true, isGrounded, isJumping;
     private Vector3 moveDirection = Vector3.zero;
-    private float cameraPitch = 0;
+    private float cameraPitch = 0, jumpTimer;
 
-    private bool jumpPressed, isRunning;
+    private bool jumpPressed, runPressed;
     private float xAxis = 0f, yAxis = 0f, mouseX = 0f, mouseY = 0f;
 
     //? References
@@ -50,6 +51,7 @@ public class PlayerMovement : MonoBehaviour {
         UpdateInputs();
         if(canMove) {
             HandleCameraMovement();
+            HandleJump();
             FixMovement();
         }
     }
@@ -84,7 +86,7 @@ public class PlayerMovement : MonoBehaviour {
         // Limit player velocity
         var curVel = rb.velocity; curVel.y = 0;
 
-        var speed = isRunning ? runSpeed : walkSpeed;
+        var speed = runPressed ? runSpeed : walkSpeed;
         if (curVel.magnitude > speed) {
             var correctedVel = curVel.normalized * speed;
             correctedVel.y = rb.velocity.y;
@@ -97,20 +99,35 @@ public class PlayerMovement : MonoBehaviour {
         if(canMove) {
             //? MOVE
             moveDirection = (transform.forward * xAxis) + (transform.right * yAxis);
-            
-            //? JUMP
-            var mvDirY = moveDirection.y;
-            // TODO: check canJump
-            //moveDirection.y = jumpPressed && isGrounded ? jumpForce : mvDirY;
-            // TODO: if (!grounded)
-            //moveDirection.y -= (jumpPressed ? jumpGravity : fallGravity);
 
             // Apply forces
+            var gravity = jumpPressed && isJumping ? jumpGravity : fallGravity;
+            rb.AddForce(Physics.gravity * gravity * rb.mass);
             rb.AddForce(moveDirection.normalized * moveForce, ForceMode.Force);
-            
 
             // TODO: https://www.youtube.com/watch?v=qQLvcS9FxnY
             // TODOv2: https://www.youtube.com/watch?v=f473C43s8nE
         }
+    }
+
+    void HandleJump() {
+        //? JUMP
+        if (jumpPressed && isGrounded) {
+            // Start jumping 
+            if (isGrounded) {
+                rb.AddForce(transform.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
+                jumpTimer = maxJumpTime;
+                isJumping = true;
+            }
+            // Make higher jump is input was held
+            else if (isJumping) {
+                if (jumpTimer > 0) {
+                    
+                    jumpTimer -= Time.deltaTime;
+                }
+                else isJumping = false;
+            }
+        }
+        else isJumping = false;
     }
 }
