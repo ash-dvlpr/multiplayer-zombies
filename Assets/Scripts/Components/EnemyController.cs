0cd5100583384b9abc4e52f164305957f9fc78da
Λsh Dev <69819379+ash-dvlpr@users.noreply.github.com>
2023-12-06 17:43:08 +0100

    Basic enemy behaviour

--- Assets/Scripts/Components/EnemyController.cs
+++ Assets/Scripts/Components/EnemyController.cs
@@ -1,15 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
+    // ====================== References =====================
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Transform _target;
 
+    // ====================== Unity Code ======================
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update() {
+    void Start() {
+        ChangeTarget();
+    }
 
+    void Update() {
+        if (null != _target) {
+            agent.destination = _target.position;
+        }
+
+        bool moving = agent.velocity.magnitude > 1;
+        animator.SetBool(AnimatorID.isRunning, moving);
+    }
+
+    // ===================== Custom Code =====================
+    private void ChangeTarget() {
+        GameObject[] players;
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+
+        GameObject nearestPlayer = null;
+
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        foreach (GameObject p in players) {
+            var disatance = p.transform.position - position;
+            float curDistance = disatance.sqrMagnitude;
+
+            if (curDistance < distance) {
+                nearestPlayer = p;
+                distance = curDistance;
+            }
+        }
+
+        ChangeTarget(nearestPlayer.transform);
+    }
+
+    public void ChangeTarget(Transform newTarget) {
+        _target = newTarget;
     }
 }
---
+++ Assets/Scripts/Managers/AnimatorID.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimatorID {
+    public static readonly int isMoving = Animator.StringToHash("IsMoving");
+    public static readonly int isRunning = Animator.StringToHash("IsRunning");
+    public static readonly int triggerAttack = Animator.StringToHash("AttackTrigger");
+
+
+}
